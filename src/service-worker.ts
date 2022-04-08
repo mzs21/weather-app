c89d3bf774ec268a76aa305a6edb3ef4cf552390
mzs21 <snigdho2111@gmail.com>
2022-04-08 10:34:52 +0600

    Working with Offline.html

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -79,3 +79,51 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
+
+
+const CACHE_NAME = "cache";
+const urlsToCache = ["../public/index.html", "../public/offline.html"];
+const version = "v0.0.1";
+
+//install sw at first time
+//place to cache assets to speed up the loading time of web page
+
+self.addEventListener("install", (event: any) => {
+  console.log("sw install event");
+  event.waitUntil(
+    caches.open(version + CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+
+//Activate the sw after install
+//Place where old caches are cleared
+self.addEventListener("activate", (event: any) => {
+  console.log("sw activate event");
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return cacheName.indexOf(version) !== 0;
+          })
+          .map(function (cachName) {
+            return caches.delete(cachName);
+          })
+      )
+    )
+  );
+});
+
+
+//listen for requests
+self.addEventListener("fetch", (event: any) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
